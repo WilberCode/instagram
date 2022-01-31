d31110e0716e350112eb77ebad68932837633b07
wilber <parionahuaraca@gmail.com>
2022-01-31 18:38:00 -0500

    cambios

--- components/mulecules/Main.js
+++ components/mulecules/Main.js
@@ -5,7 +5,7 @@ import Stories from "./Stories"
 
 const Main = () => {
     return (
-        <main  className="w-full max-w-[614px]  " > 
+        <main  className="w-full max-w-[614px] mb-8  " > 
             <Stories/>
             <Posts key={2}/>
 
